9118c9dd37ffbf7c5d2a7e96f2456bb032f1e897
Breanna Burris <breanna1burris@gmail.com>
2024-09-08 21:36:30 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -7,4 +7,21 @@ function calculateAverageSales (salesFigures) {
     return averageSales;   
 }
 
+//Function to Determine Performance Rating
 
+function determinePerformanceRating(averageSales) {
+  
+
+    if (averageSales > 10000) {
+        return "Excellent";
+    }
+    else if (averageSales >= 7000) {
+        return "Good";
+    }
+    else if (averageSales >= 4000) {
+        return "Satisfactory";
+    }
+    else{
+        return "Needs Improvement";
+    }
+} //performance rating funtion depending on average sales amount
